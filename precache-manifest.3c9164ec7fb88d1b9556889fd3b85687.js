92563305822ea2f5cda898d3262242c902b6f45f
Ramganesh <ramg9048@gmail.com>
2020-01-15 14:51:41 +0530

    Updates

---
+++ precache-manifest.3c9164ec7fb88d1b9556889fd3b85687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca1bb0afe2654c611457d952180135d",
+    "url": "/monster-search/index.html"
+  },
+  {
+    "revision": "2914bdcb35a5cc7e914c",
+    "url": "/monster-search/static/css/main.5f1b10fa.chunk.css"
+  },
+  {
+    "revision": "42cba20329b3e5d0e6ad",
+    "url": "/monster-search/static/js/2.11cea66e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-search/static/js/2.11cea66e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2914bdcb35a5cc7e914c",
+    "url": "/monster-search/static/js/main.2425d6a8.chunk.js"
+  },
+  {
+    "revision": "860d89880c5e3c07b3b0",
+    "url": "/monster-search/static/js/runtime-main.3c7da69e.js"
+  }
+]);
